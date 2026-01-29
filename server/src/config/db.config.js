2f78581e37ffc15940e06eaff03d083228e30c64
Aayush Bhattarai <Aayush206a@gmail.com>
2026-01-29 18:02:23 +0545

    Add server as normal folder

---
+++ server/src/config/db.config.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+export const connectDb = () => {
+    console.log(process.env.DB_URI)
+
+    mongoose.connect(process.env.DB_URI ,{
+        dbName:process.env.DB_NAME,
+        autoCreate: true
+    })
+    .then(() => {
+        console.log("database created");
+    })
+    .catch((err) => {
+        console.log("Database connection error");
+        console.log(err)
+    })
+
+}
+//! mongodb://localhost:27017
+    // mongoose.connect()
---
+++ server/src/routes/auth.routes.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import { login, register } from '../controllers/auth.controllers.js'
+
+const router = express.Router()
+
+const middelware = (req, res, next) => {
+    console.log('route level')
+    next()
+}
+//? register router
+router.post('/register', register);
+
+//? login
+router.post('/login',middelware,middelware,middelware,login)
+
+export default router
